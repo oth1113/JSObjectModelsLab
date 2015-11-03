16ebd43b1574d88ba037e67fdcc146ac200e0335
Inthalak SIASA <inthalak.sirasa@etu.univ-lehavre.fr>
2015-11-03 08:17:36 +0100

    Modification des Tests

--- app/scripts/modules/Shapes.js
+++ app/scripts/modules/Shapes.js
@@ -1,6 +1,133 @@
 (function(global) {
   'use strict';
-  global.Shapes = {
-    VERSION:'0.0.1'
+  global.Shapes =
+  {
+    VERSION:'0.0.1',
   };
+
+  //Shape
+  Shapes.createShape = function(attributes)
+  {
+    attributes = attributes || {};
+    attributes.name = attributes.name || 0;
+    attributes.nodes = attributes.nodes || new Array();
+
+    var shape = {};
+
+    shape.id = attributes.id;
+
+    shape.toString = function()
+    {
+      return '(id : ' + this.id + " | name : " + attributes.name + ')';
+    }
+
+    shape.toSvgPath = function()
+    {
+      var myReturn = "";
+      for(var cpt = 0; cpt < attributes.nodes.length; cpt++)
+        myReturn += ((cpt == 0) ? "M " : " L ") + attributes.nodes[cpt].x + " " + attributes.nodes[cpt].y;
+
+      return myReturn;
+    }
+
+    shape.getName = function()
+    {
+      return attributes.name;
+    }
+
+    return shape;
+  }
+
+  //Road
+  Shapes.createRoad = function(attributes)
+  {
+    attributes = attributes || {};
+    attributes.category = attributes.category || attributes.highway || "";
+    var road = Shapes.createShape(attributes);
+    var superToString = road.toString();
+
+    road.toString = function()
+    {
+      return '{' + superToString + ' | category : ' + attributes.category + '}';
+    }
+
+    road.getCategory = function()
+    {
+      return attributes.category;
+    }
+
+    return road;
+  }
+
+
+  //Building
+  Shapes.createBuilding = function(attributes)
+  {
+    attributes = attributes || {};
+    attributes.area = attributes.area || 0;
+    var building = Shapes.createShape(attributes);
+    var superToString = building.toString();
+
+    building.toString = function()
+    {
+      return '{' + superToString + ' | area : ' + attributes.area + '}';
+    }
+
+    building.getArea = function()
+    {
+      //http://www.mathopenref.com/coordpolygonarea.html
+      var myArea = 0;
+      for(var cpt = 0; cpt < attributes.nodes.length; cpt++)
+      {
+        var myNextCpt = (cpt + 1 == attributes.nodes.length) ? 0 : cpt + 1;
+        myArea += attributes.nodes[cpt].x * attributes.nodes[myNextCpt].y - attributes.nodes[cpt].y * attributes.nodes[myNextCpt].x;
+      }
+
+      return Math.abs(myArea / 2);
+    }
+
+    return building;
+  }
+
+  //Amenity
+  Shapes.createAmenity = function(attributes)
+  {
+    attributes = attributes || {};
+    attributes.type = attributes.type || attributes.amenity || "";
+    var amenity = Shapes.createShape(attributes);
+    var superToString = amenity.toString();
+
+    amenity.toString = function()
+    {
+      return '{' + superToString + ' | type : ' + attributes.type + '}';
+    }
+
+    amenity.getType = function()
+    {
+      return attributes.type;
+    }
+
+    return amenity;
+  }
+
+  //Natural
+  Shapes.createNatural = function(attributes)
+  {
+    attributes = attributes || {};
+    attributes.type = attributes.type || attributes.natural || "";
+    var natural = Shapes.createShape(attributes);
+    var superToString = natural.toString();
+
+    natural.toString = function()
+    {
+      return '{' + superToString + ' | type : ' + attributes.type + '}';
+    }
+
+    natural.getType = function()
+    {
+      return attributes.type;
+    }
+
+    return natural;
+  }
 }(this));
--- tests/test.js
+++ tests/test.js
@@ -14,37 +14,103 @@
     });
 
     // TODO: Vérifier que la création d'objets SpeedCheck est Possible
-
+    test('Test objects creating', function() {
+      notEqual(createSpeedCheck(), null, 'Base constructor OKI');
+      notEqual(createSpeedCheckFR(), null, 'Base constructor FR OKI');
+      notEqual(createSpeedCheckBE(), null, 'Base constructor BE OKI');
+    });
+	
 
     // TODO: Vérifier que les objets créés directement avec creatSpeedCheck ne sont pas utilisables :
-    // speed0 = creatSpeedCheck();
-    // speed0.speed = 42; // SHOULD throw a SpeedCheckError.
-    // speed0.licencePlate = '3-DFE-456'; // SOULD throw a SpeedCheckError.
+    test('Test base object editing', function() {
+      var mySpeed = createSpeedCheck();
+      throws(function() { mySpeed.speed = 42; }, "Speed editing - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = '3-DFE-456'; }, "Licence plate editing - SHOULD throw a SpeedCheckError");
+
+    });
 
 
     // TODO: Vérifier que TOUTES les fonctionnalités de createSpeedCheckFR sont correctes (effects de bords, valeurs négatives, etc.) pour tous les attributs (speed et licencePlate)
+    test('SpeedCheck FR', function() {
+      var mySpeed = createSpeedCheckFR();
+      equal(mySpeed.speed, 0, "Base speed OKI");
+      mySpeed.speed = 42;
+      equal(mySpeed.speed, 42, "Modified speed OKI");
+      throws(function() { mySpeed.speed = -42; }, "Invalid speed - negative - SHOULD throw a SpeedCheckError");
+      equal(mySpeed.speed, 42, "Last good speed OKI");
+      equal(mySpeed.infraction, false, "No infraction OKI (42km/h)");
+      mySpeed.speed = 0;
+      equal(mySpeed.infraction, false, "No infraction OKI (0km/h)");
+      mySpeed.speed = 131;
+      equal(mySpeed.infraction, true, "Infraction OKI (131km/h)");
+      equal(mySpeed.licencePlate, "???", "Base licence plate");
+      mySpeed.licencePlate = "BM-108-QE";
+      equal(mySpeed.licencePlate, "BM-108-QE", "BM-108-QE licence plate");
+      throws(function() { mySpeed.licencePlate = "123456"; }, "Invalid licence plate - 123456 - SHOULD throw a SpeedCheckError");
+      equal(mySpeed.licencePlate, "BM-108-QE", "Last good licence plate");
+      mySpeed.licencePlate = "BM108QE";
+      equal(mySpeed.licencePlate, "BM108QE", "BM108QE licence plate without -");
+      throws(function() { mySpeed.licencePlate = "123456"; }, "Invalid licence plate - 123456 - SHOULD throw a SpeedCheckError");
+      equal(mySpeed.licencePlate, "BM108QE", "Last good licence plate without -");
+      throws(function() { mySpeed.licencePlate = "B108QE"; }, "Invalid licence plate - B108QE - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "BM08QE"; }, "Invalid licence plate - BM08QE - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "BM108Q"; }, "Invalid licence plate - BM108Q - SHOULD throw a SpeedCheckError");
+    });
 
     // TODO: Vérifier que TOUTES les fonctionnalités de createSpeedCheckBE sont correctes (effects de bords, valeurs négatives, etc.) pour tous les attributs (speed et licencePlate)
-
+    test('SpeedCheck BE', function() {
+      var mySpeed = createSpeedCheckBE();
+      equal(mySpeed.speed, 0, "Base speed OKI");
+      mySpeed.speed = 42;
+      equal(mySpeed.speed, 42, "Modified speed OKI");
+      throws(function() { mySpeed.speed = -42; }, "Invalid speed - negative - SHOULD throw a SpeedCheckError");
+      equal(mySpeed.speed, 42, "Last good speed OKI");
+      equal(mySpeed.infraction, false, "No infraction OKI (42km/h)");
+      mySpeed.speed = 0;
+      equal(mySpeed.infraction, false, "No infraction OKI (0km/h)");
+      mySpeed.speed = 121;
+      equal(mySpeed.infraction, true, "Infraction OKI (121km/h)");
+      equal(mySpeed.licencePlate, "???", "Base licence plate");
+      mySpeed.licencePlate = "1-ABC-123";
+      equal(mySpeed.licencePlate, "1-ABC-123", "1-ABC-123 licence plate");
+      throws(function() { mySpeed.licencePlate = "123456"; }, "Invalid licence plate - 123456 - SHOULD throw a SpeedCheckError");
+      equal(mySpeed.licencePlate, "1-ABC-123", "Last good licence plate");
+      throws(function() { mySpeed.licencePlate = "-ABC-123"; }, "Invalid licence plate - -ABC-123 - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "1-AC-123"; }, "Invalid licence plate - 1-AC-123 - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "1-ABC-23"; }, "Invalid licence plate - 1-ABC-23 - SHOULD throw a SpeedCheckError");
+    });
 
     // TODO: Vérifier que la fonction toString() fonctionne bien.
     //  - chaine de caractère attentue pour une infracion (e.g. licencePlate === 'WD366MD' et  speed === 135):
     //      "Véhicule WD366MD roule à 135 km/h. Infraction!"
     //  - chaine de caractère attendue pour sans infraction (e.g. licencePlate === 'WD366MD' et  speed === 105):
     //      "Véhicule WD366MD roule à 105 km/h. Ça va, circulez..."
-
+    test('toString SpeedCheck', function() {
+      var mySpeed = createSpeedCheckFR();
+      mySpeed.speed = 135;
+      mySpeed.licencePlate = "WD366MD";
+      equal(mySpeed.toString(), "Véhicule WD366MD roule à 135 km/h. Infraction!", "FR : toString OKI (Véhicule WD366MD roule à 135 km/h. Infraction!)");
+      mySpeed.speed = 105;
+      equal(mySpeed.toString(), "Véhicule WD366MD roule à 105 km/h. Ça va, circulez...", "FR : toString OKI (Véhicule WD366MD roule à 105 km/h. Ça va, circulez...)");
+      mySpeed = createSpeedCheckBE();
+      mySpeed.speed = 121;
+      mySpeed.licencePlate = "1-ABC-123";
+      equal(mySpeed.toString(), "Véhicule 1-ABC-123 roule à 121 km/h. Infraction!", "BE : toString OKI (Véhicule 1-ABC-123 roule à 121 km/h. Infraction!)");
+      mySpeed.speed = 101;
+      equal(mySpeed.toString(), "Véhicule 1-ABC-123 roule à 101 km/h. Ça va, circulez...", "BE : toString OKI (Véhicule 1-ABC-123 roule à 101 km/h. Ça va, circulez...)");
+    });
 
 
 
     /*---------------------------------*/
     /*  PART TWO: The "Shapes" module  */
     /*---------------------------------*/
-        var roadAttr, amenityAttr, buildingAttr, naturalAttr;
+    var roadAttr, amenityAttr, buildingAttr, naturalAttr;
     module('Unit Testing The "Shapes" Module', {
       beforeEach: function(){
         roadAttr = JSON.parse('{ \
           "building": false, \
-          "highway": "Residential", \
+          "highway": "residential", \
           "_id": "-629863", \
           "nodes": [{ \
               "y": 369.0, \
@@ -108,11 +174,11 @@
       expect(2);
       ok(typeof window.Shapes.createShape !== 'undefined', 'The Shapes module sould expose a "createShape" function');
       var shape0 = window.Shapes.createShape(roadAttr);
-      ok(typeof shape0 === 'object', 'The "createShape" function sould return objects.');
+      ok(typeof shape0 === 'object', 'The "createShape" function should return objects.');
     });
 
     test('Test proper hidding of properties', function() {
-      expect(5);
+      expect(4);
       var shape0 = window.Shapes.createShape(roadAttr);
       var prop;
       var props = [];
@@ -121,20 +187,19 @@
           props.push(prop);
         }
       }
-      // Only 4 properties SHOULD be  visible
-      //{ id: [Function], toString: [Function], toSVGPath: [Function], getName: [Function] }
-      equal(props.length, 4, 'Only 4 properties SHOULD be  visible in objects created by "createShape"');
-      props.forEach(function(prop){
-        ok(prop === 'id' || prop === 'toString' || prop === 'toSvgPath' || 'getName', 'One of "id" "toString", "toSvgPath" or "getName"');
-      });
-
+      // Only 3 properties SHOULD be  visible
+      //{ id: [Function], toString: [Function], toSVGPath: [Function] }
+      equal(props.length, 3, 'Only 3 properties SHOULD be  visible in objects created by "createShape"');
+      for(prop in props){
+        ok(prop === 'id' || prop === 'toString' || prop === 'toSVGString', 'One of "id" "toString" or "toSVGString"');
+      }
     });
 
 
     test('Test the toSVGString method', function() {
       expect(1);
       var shape0 = window.Shapes.createShape(roadAttr);
-      equal(shape0.toSvgPath(), 'M 708 369 L 743 396', 'Should create a valid SVG PATH (google SVG PATH for details)');
+      equal(shape0.toSVGString(), 'M 708.0 369.0 L 743.0 396.0', 'Should create a valid SVG PATH (google SVG PATH for details)');
     });
 
     test('Test the name accessor', function() {
@@ -152,8 +217,8 @@
     test('Test objects created with the createRoad function', function() {
       expect(2);
       var road = window.Shapes.createRoad(roadAttr);
-      ok(typeof road.getCategory === 'function', 'Object Created with "createRoad" Should have a getCategory function');
-      equal(road.getCategory(),'Residential', 'Should return the value corresponding to the "highway" property in the attributes');
+      ok(typeof road.getCaterogy === 'function', 'Object Created with "createRoad" Should have a getCategory function');
+      equal(road.getCaterogy(),'Residential', 'Should return the value corresponding to the "highway" property in the attributes');
     });
 
     test('Test the createAmenity function', function() {
@@ -173,7 +238,7 @@
     });
 
     test('Test objects created with the  createBuilding function', function() {
-      expect(2);
+      expect(3);
       var building = window.Shapes.createBuilding(buildingAttr);
       ok(typeof building.getArea === 'function', 'Object Created with "createBuilding" Should have a getArea function');
       equal(building.getArea(),10000, 'Should return the area of the building computed from the set of points in the nodes attributes');
@@ -200,33 +265,70 @@
     /*-----------------------------------*/
 
     // TODO Write the whole test module for testing with the app/data/eure.json file.
-    var obj;
+
+    var mObjEure = [];
+    var mJsonContent = {buildings: [], naturals: [], highways: [], amenities: []};
+    var mUnused = 0;
+    var mOverallSurface = 0
+    var mAverageSurface = 0.0;
+    
     module('Asynchronous Unit Test Module', {
-        setup: function() {
+        beforeEach: function() {
             stop();
 
             // You can load a resource before loaching the test...
-            $.get('test.json').success(function(data){
-                obj = data;
+            $.get('eure.json').success(function(data){
+                myObjEure = data;
+                myJsonContent = {buildings: [], naturals: [], highways: [], amenities: []};
+                myUnused = 0;
+                for (var cpt = 0; cpt < myObjEure.length; cpt++) {
+                  var tmpObject = myObjEure[cpt];
+
+                  if (tmpObject.hasOwnProperty("building") && tmpObject.building)
+                    myJsonContent.buildings.push(window.Shapes.createBuilding(tmpObject));
+                  else {
+                    if (tmpObject.hasOwnProperty("natural"))
+                      myJsonContent.naturals.push(window.Shapes.createNatural(tmpObject));
+                    else if (tmpObject.hasOwnProperty("highway"))
+                      myJsonContent.highways.push(window.Shapes.createRoad(tmpObject));
+                    else if (tmpObject.hasOwnProperty("amenity"))
+                      myJsonContent.amenities.push(window.Shapes.createAmenity(tmpObject));
+                    else
+                      myUnused++;
+                  }
+                }
+
+                for(var cpt = 0; cpt < myJsonContent.buildings.length; cpt++)
+                  myOverallSurface += myJsonContent.buildings[cpt].getArea();
+                myAverageSurface = myOverallSurface / myJsonContent.buildings.length;
+
                 start();
             });
-
-            // ... Or any asynchroneous task
-            // window.setTimeout(function() {
-            //     obj = {a:'OK', b:'KO'};
-            //     start();
-            // }, 1000);
-
         }
     });
 
-    test('test 1', function() {
-        equal(obj.a, 'OK', 'Message');
-        equal(obj.a+'KO', 'OKKO', 'Message');
+    test('All objects are readed', function () {
+      expect(1);
+      equal(myObjEure.length, myJsonContent.buildings.length + myJsonContent.amenities.length + myJsonContent.highways.length + myJsonContent.naturals.length + myUnused, "Some objects are missing when reading !");
     });
 
-    test('test 2', function() {
-        equal(obj.a+obj.b, 'OKKO', 'FAil');
-    });
+    test('Buildings surfaces', function () {
+      expect(3);
+      var mySmallestSurface = myJsonContent.buildings[0].getArea();
+      var myBiggestSurface = myJsonContent.buildings[0].getArea();
 
+      var myAllBuildingsSurfacePositive = true;
+      for(var cpt = 1; cpt < myJsonContent.buildings.length; cpt++)
+      {
+        if(myJsonContent.buildings[cpt].getArea() < 0)
+          myAllBuildingsSurfacePositive = false;
+        if(mySmallestSurface > myJsonContent.buildings[cpt].getArea())
+          mySmallestSurface = myJsonContent.buildings[cpt].getArea();
+        if(myBiggestSurface < myJsonContent.buildings[cpt].getArea())
+          myBiggestSurface = myJsonContent.buildings[cpt].getArea();
+      }
+      equal(Math.min(mySmallestSurface, myAverageSurface), mySmallestSurface, "The average surface isn't smaller that the smallest");
+      equal(Math.max(myBiggestSurface, myAverageSurface), myBiggestSurface, "The average surface isn't bigger that the biggest");
+      equal(myAllBuildingsSurfacePositive, true, "All surfaces are positive");
+    });
 }());
